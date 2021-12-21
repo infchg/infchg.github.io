837cd17a4ed08651bf8d3f7ed662f5f1ae8cd5a9
Information-Change jc <infchg.appspot@gmail.com>
2021-12-21 17:35:28 +0100

    Add files via upload

---
+++ data/urls-iso3.js
@@ -0,0 +1,175 @@
+ls= //remove line for json
+{
+  "usage": " yq . 2prod/geo-gis/data/urls-iso3.md  --yaml-output  ",
+  "usage2": " ( echo ls= //remove line for json &&  yq . 2prod/geo-gis/data/urls-iso3.md ) > 2prod/geo-gis/data/urls-iso3.js",
+  "desc": "YAML DE DATABASE O SEARCH  Y   TEMAS",
+  "lis": {
+    "AFG": {
+      "href": "https://drive.google.com/drive/folders/1rXwLCNR70qqEHFbowqRkoNc7fcVz0NHm?jc=AFGANIST"
+    },
+    "AGO": {
+      "href": "https://drive.google.com/drive/folders/1hVmvR1WdaetlUHc0RNTELpIfG-E1TlQJ?jc=ANGOLA"
+    },
+    "SAU": {
+      "href": "https://drive.google.com/drive/folders/1Q6E9wKTxEBZNdo_VdBYUWz-YzK7AZzUr?jc=ARABIAS"
+    },
+    "DZA": null,
+    "ARM": {
+      "href": "https://drive.google.com/drive/folders/1AVbRXP7BhjNUppPVtMax-tB8RsSlKeUZ?jc=ARMENIA"
+    },
+    "BFA": {
+      "href": "https://drive.google.com/drive/folders/1Q_5BYvB-VQ3z8yJo4VY1s6YrxF1DUDOf?jc=BURKINA",
+      "wb": "HVO"
+    },
+    "CMR": {
+      "href": "https://drive.google.com/drive/folders/1_aTF39s_HSq7FihrssCevDOhMLcPFpB4?jc=CAMERUN"
+    },
+    "COL": {
+      "href": "https://drive.google.com/drive/folders/1Dd46RZ4WnSSZz12zmIqqcaE_ARDcywQq?jc=COLOMBIA",
+      "busq": true
+    },
+    "CIV": {
+      "href": "https://drive.google.com/drive/folders/14c3l1e2ZMLRKEg5J0hL65C1XXnRzGyKV?jc=COSTADE"
+    },
+    "CUB": {
+      "href": "https://drive.google.com/drive/folders/1CMFrzeO2rx4Cs7ZtXdAqlo6FVz_q9Yzs?jc=CUBA"
+    },
+    "SLV": {
+      "href": "https://drive.google.com/drive/folders/1HuoMGUPU_-4zKS3Kw_Ac5zfbDEzxPX0N?jc=ELSALVA",
+      "busq": true
+    },
+    "ERI": {
+      "href": "https://drive.google.com/drive/folders/1t7HqQaM8ferTKQ8i7S_Yj9shFbx18WRD?jc=ERITREA"
+    },
+    "PHL": {
+      "href": "https://drive.google.com/drive/folders/1BerZUek1-Zl34nmGB8vYbsEw8_IELJaD?jc=FILIPINA"
+    },
+    "GMB": {
+      "href": "https://drive.google.com/drive/folders/1qIohCv-IBoDuLUwGjZPwRCLpcVwl4L0h?jc=GAMBIA"
+    },
+    "GEO": {
+      "href": "https://drive.google.com/drive/folders/17AtyuhxWdoL9SVd9IFjC6xgeyEFtZiMC?jc=GEORGIA"
+    },
+    "GHA": {
+      "href": "https://drive.google.com/drive/folders/1p2udlEEPlUt5d_brZl_cqpvmrAxGs8eL?jc=GHANA"
+    },
+    "GIN": {
+      "href": "https://drive.google.com/drive/folders/1aC7mWRTbMHrrJAS60t2NAZ7yb7aAAf6k?jc=GUINEAC"
+    },
+    "GNQ": {
+      "href": "https://drive.google.com/drive/folders/18FCrUwKhOUGNMcuwTHb13VM6oKZL4unr?jc=GUINEAE"
+    },
+    "HND": {
+      "href": "https://drive.google.com/drive/folders/1RPaXShHcQIRWxCvqY98gvHmUOPOJc25C?jc=HONDURAS",
+      "d": 2,
+      "busq": true
+    },
+    "IRQ": {
+      "href": "https://drive.google.com/drive/folders/1EcXEy4eN2TaHksSTukabVrMQ85XSglOR?jc=IRAK"
+    },
+    "IRN": {
+      "href": "https://drive.google.com/drive/folders/1mtZv6WfOZPWjNE4-FkHBCqfDSTqQlbG6?jc=IRAN"
+    },
+    "JAM": {
+      "href": "https://drive.google.com/drive/folders/1BEx4zEf5VM6iIOU1VZ9fsJL2d3v_-gNz?jc=JAMAICA"
+    },
+    "JOR": {
+      "href": "https://drive.google.com/drive/folders/1n1v5x0Xrk2O17p4aL1K-oGx8ZZnhJWKf?jc=JORDANIA"
+    },
+    "LBN": {
+      "href": "https://drive.google.com/drive/folders/1I9panW_cDG5zGI6BWoxhM1iBZfFjoa9P?jc=LIBANO"
+    },
+    "MLI": {
+      "href": "https://drive.google.com/drive/folders/1Gh46mrZDQ8AtVJ4aCy5GOyovxYdXnL79?jc=MALI",
+      "nat": "1I9hj2E17rppv40E4NpDJh9tO8aBWYDGA Deportaciones",
+      "rac": "1NG4imwFS9rfUUZeKGeBVrgCIRa9hR3NT Esclavitud Etnias",
+      "rel": "1WYn1GaE1aOsA9yNgKmRES-O2io8lUfVk Persecución a cristianos",
+      "pol": "1VnNRAKJt0H_26NlVqGVL6cPM0QAO1LrE Golpe de Estado",
+      "grpm": null,
+      "img": "https://www.rcinet.ca/en/wp-content/uploads/sites/3/2018/03/peacekeeping-mail-4-dead.jpg",
+      "busq": true
+    },
+    "MAR": {
+      "href": "https://drive.google.com/drive/folders/1MFUT5iYubOYIFgKbL9KNaKsAe23vBnvA?jc=MARRUECO"
+    },
+    "MRT": {
+      "href": "https://drive.google.com/drive/folders/14kCBmlkHWhjulRr7cauz5Ewz5rK81eUT?jc=MAURITAN"
+    },
+    "MMR": {
+      "href": "https://drive.google.com/drive/folders/1m_ig65p1g3i2xnTwJMfO5XN3yK4xwc2c?jc=MYAMAR",
+      "wb": "BUR"
+    },
+    "NIC": {
+      "href": "https://drive.google.com/drive/folders/1zwEWAo97smNYNWy95mPjZrlMEVB6Rm_-?jc=NICARAGU",
+      "busq": true
+    },
+    "NGA": {
+      "href": "https://drive.google.com/drive/folders/1C7TtNMXUt_Wgp27YSAcS2J9dl_I_25F1?jc=NIGERIA"
+    },
+    "PAK": {
+      "href": "https://drive.google.com/drive/folders/11NzkXKaWw0bm0nN2XEYO6b5y8E4ckhf5?jc=PAKISTA"
+    },
+    "PSE": {
+      "href": "https://drive.google.com/drive/folders/1oYsxzspDgIELzPX-d-reTqeOFQpgOCQs?jc=PALESTIN",
+      "wb": "ISR"
+    },
+    "PRY": null,
+    "PER": {
+      "href": "https://drive.google.com/drive/folders/15rw8QlfQoosIh2aj0ggR0_ce63Uktaj_?jc=PERU"
+    },
+    "COD": {
+      "href": "https://drive.google.com/drive/folders/1ou-t5I7V-LU28UEV3WZYhth9XQqRtwXn?jc=R.DCONG",
+      "wb": "ZAR"
+    },
+    "RUS": {
+      "href": "https://drive.google.com/drive/folders/19qQgPyTKcE3uoWQ92UNzHWQSYLXaqqau?jc=RUSIA",
+      "grpm": "1aWR1znQytn0kCMrFU-NngwFzL4ctO2F2 Persecución por orientación sexual"
+    },
+    "SEN": {
+      "href": "https://drive.google.com/drive/folders/1r5wAXNwSxR0BySZovDIlN3U1gvGKlwhw?jc=SENEGAL",
+      "d": 2
+    },
+    "SYR": {
+      "href": "https://drive.google.com/drive/folders/1JFExg8EKK7XJx_6HeXfpKQWJOIe52feY?jc=SIRIA"
+    },
+    "SOM": {
+      "href": "https://drive.google.com/drive/folders/1rQNynjTW6XGSPeyznQ61VuUF8ipcO-oz?jc=SOMALIA"
+    },
+    "SDN": {
+      "href": "https://drive.google.com/drive/folders/12NBU7vzPcfGNiB55W9lOUnvNa2lIRByA?jc=SUDAN"
+    },
+    "SSD": {
+      "href": "https://drive.google.com/drive/folders/1DvcDWkeq9fI9rsgrInXcWEmvgFJvjqXO?jc=SUDAND"
+    },
+    "TUN": {
+      "href": "https://drive.google.com/drive/folders/1Xe_rOXbXC7Tdkd2ucgsDk-0STDoNxpYw?jc=TUNEZ"
+    },
+    "TUR": {
+      "href": "https://drive.google.com/drive/folders/1okj7rRy9nSZpIJCk_3A1fDKiM_BoY9mo?jc=TURQUIA",
+      "d": 2
+    },
+    "ESH": {
+      "img": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F80%2F06%2Ffe%2F8006feec0a83b91912fc77745a270d40--western-sahara-refugee-camps.jpg "
+    },
+    "VEN": {
+      "href": "https://drive.google.com/drive/folders/1Wu402DGrX5j-9qxLapiu8nKTmoP3yecB?jc=VENEZUEL",
+      "rac": "1Gpcms6iBkUD75kC5VF61PTN1v_8eG_3G   Minoría china",
+      "grph": "1Ambh4Z1eBBu8qjzdriQg13ys2eeKuvPM   Falta medicinas",
+      "grpa": "15ITtGWib_s_kgAhuivb25XZDz7NuVbuv   Prisiones/Funcionarios",
+      "grpm": "1sWzw4UX9EpxF4U72afdyWZut7pkP2EHv   LGTBI+",
+      "pol": "1aHSP_w8lR4DG46rPYPtA3FjlBooJoTLg   Adoctrinamiento",
+      "busq": "1VouR5DOIQr0kZ_t0V3teCd2SPpbg0y8N   VIOLACIONES Y ABUSOS",
+      "img": "https://e.rpp-noticias.io/normal/2018/02/22/452145_568878.jpg"
+    },
+    "YEM": {
+      "href": "https://drive.google.com/drive/folders/11110pZPJYb0S2KZIhPOk1ZFnsNwcwL4S?jc=YEMEN"
+    }
+  },
+  "exp": [
+    {
+      "pol": "url2pathname"
+    },
+    "url3a",
+    "url4"
+  ]
+}
